8ec73fe8f3c0032c990701ce304f4cf98a685dce
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2021-01-14 09:40:11 -0500

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+DWORD pID;
+DWORD foodADD = 0xDC3eFCC4;
+int food;
+
+int nFood = food += 20;
+
+int main()
+{
+	cout << "PRESS THE UP ARROW KEY!" << endl;
+
+	while (true)
+	{
+		if (GetAsyncKeyState(VK_UP))
+		{
+			for (;;)
+			{
+				HWND hwnd = FindWindowA(0, ("Minecraft 1.16.4 - Singleplayer"));
+				if (!hwnd) cerr << "CANNOT FIND MINECRAFT 1.16.4" << endl;
+
+				GetWindowThreadProcessId(hwnd, &pID);
+				HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pID);
+				if (!pHandle) cerr << "ERROR" << endl;
+				ReadProcessMemory(pHandle, (LPVOID)foodADD, &food, (DWORD)sizeof(food), 0);
+
+
+				WriteProcessMemory(pHandle, (LPVOID)foodADD, &nFood, (DWORD)sizeof(nFood), 0);
+
+
+		}
+	}
+	system("pause");
+	return 1;
+}
